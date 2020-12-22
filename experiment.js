072a91df44672c61c959f91b668248aca6edc2f2
lfelipearaujo <63312737+lfelipearaujo@users.noreply.github.com>
2020-12-22 17:09:21 -0500

    u1
    I have followed the guidelines in the course, plus added another page called mountains.

---
+++ experiment.js
@@ -0,0 +1,33 @@
+//! Animation based on scroll
+
+// const hikeExp = document.querySelector(".hike-exp");
+
+// window.addEventListener("scroll", scrollReveal);
+
+// function scrollReveal() {
+//   const hikePos = hikeExp.getBoundingClientRect().top;
+//     const windowHight = window.innerHeight / 1.8;
+//     if (hikePos < windowHight) {
+//         hikeExp.style.color = 'red';
+//     }
+// }
+
+//! OBserver
+
+// const slide = document.querySelector(".hike");
+
+// let options = {
+//   threshold: 0.5,
+// };
+
+// let observer = new IntersectionObserver(slideAnim, options);
+
+// function slideAnim(entries) {
+//    entries.forEach(entry => {
+//        if (entry.isIntersecting) {
+//            slide.style.background = 'salmon';
+//        }
+//     });
+// }
+
+// observer.observe(slide);
---
+++ scrollMagicExperiment.js
@@ -0,0 +1,13 @@
+//! Example using scroll Magic
+
+const controller = new ScrollMagic.Controller();
+
+const exploreScene = new ScrollMagic.Scene({
+    triggerElement: '.hike-exp',
+    triggerHook: 0.5
+})
+    .addIndicators({
+        colorStart: 'white', colorTrigger: 'white'
+    })
+    .setClassToggle('.hike-exp','active')
+    .addTo(controller);
